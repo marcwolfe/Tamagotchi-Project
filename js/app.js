d605fec41b4636b8282f3c0e0a7f2ec6a37413b3
marcwolfe <marc.wolfe21@gmail.com>
2021-12-06 09:37:18 -0600

    Inital Setup

---
+++ js/app.js
@@ -0,0 +1,20 @@
+class Pet {
+  constructor(hunger, sleepiness, boredom, age){
+    this.hunger = hunger
+    this.sleepiness = hunger
+    this.boredom = hunger
+    this.age = age
+  }
+}
+
+class Wolfie extends Pet {
+  constructor(hunger, sleepiness, boredom, age){
+    super(hunger, sleepiness, boredom, age)
+    this.hunger = 10
+    this.sleepiness = 10
+    this.boredom = 10
+    this.age = age
+  }
+}
+
+const wolfie = new Wolfie('.Wolfie')
