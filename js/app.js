3700e9b66574e5b69f725ca332b971ef2b64ff19
marcwolfe <marc.wolfe21@gmail.com>
2021-12-06 21:26:53 -0600

    Minor Updates

--- js/app.js
+++ js/app.js
@@ -17,110 +17,11 @@ class Wolfie extends Pet {
     this.boredom = 10
     this.age = 0
   }
-}
 
-
-
-const wolfie = new Wolfie('.Wolfie')
-
-const game = {
-    name: this.name,
-    hunger: 10,
-    sleepiness: 10,
-    boredom: 10,
-    age: 0,
-
-    setHunger(){
-        const intervalID = setInterval(() => {
-          const hungerID = document.querySelector('.hunger');
-          hungerID.innerText = ` ${this.hunger}`;
-          // decrement hunger
-          this.hunger--
-          hungerButton.addEventListener('click', () =>{
-              this.hunger++
-              document.body.style.backgroundImage = "url('https://64.media.tumblr.com/c4ef683055d8fc8e5517cdc221a7d57a/d3238a119a00c682-b9/s1280x1920/324855e88fa138547b29b3f87200161526dc2356.png')"
-          })
-          if(this.hunger <= 0){
-            alert('Your pet is Dead')
-
-            clearInterval(intervalID);
-            document.querySelector('button').disabled = false;
-            return;
-          }
-        }, 1000)
-      },
-      setSleepiness(){
-          const intervalID = setInterval(() => {
-            const sleepinessID = document.querySelector('.sleepiness');
-            sleepinessID.innerText = ` ${this.sleepiness}`;
-            // decrement sleepiness
-            this.sleepiness--
-            sleepinessButton.addEventListener('click', () =>{
-                this.sleepiness++
-                document.body.style.backgroundImage = "url('https://cutewallpaper.org/21/pixel-stars-background/night-nighttime-stars-clouds-background-pixel-pixel-bac.png')";
-            })
-            if(this.sleepiness <= 0){
-              alert('Your pet is Dead')
-              //update image to dead wolf or game of thrones red wedding
-              clearInterval(intervalID);
-              document.querySelector('button').disabled = false;
-              return;
-            }
-          }, 1000)
-        },
-        setBoredom(){
-          const intervalID = setInterval(() => {
-            const boredomID = document.querySelector('.boredom');
-            boredomID.innerText = ` ${this.boredom}`;
-            // decrement boredom
-            this.boredom--
-            boredomButton.addEventListener('click', () =>{
-                this.boredom++
-            })
-            if(this.boredom <= 0){
-              alert('Your pet is Dead')
-
-              clearInterval(intervalID);
-              document.querySelector('button').disabled = false;
-              return;
-            }
-          }, 1000)
-        },
-
-          setAge(){
-            const intervalID = setInterval(() => {
-              const ageID = document.querySelector('.age');
-              ageID.innerText = ` ${this.age}`;
-              // increment age
-              this.age++;
-
-              if(this.age >= 25){
-                alert('Your pet is Dead')
-
-                clearInterval(intervalID);
-                document.querySelector('button').disabled = false;
-                return;
-              }
-            }, 25000)
-          },
-
-          setName(){
-          this.name = prompt('Name your pet')
-          const nameID = document.querySelector('.name')
-          nameID.innerText = `${this.name}`
-        },
-
-
-    startGame() {
-      this.setName()
-      this.setHunger()
-      this.setBoredom()
-      this.setSleepiness()
-      this.setAge()
+  feed(){
+    this.hunger++
   }
-
 }
-
 
 let hungerButton = document.querySelector('.feedButton')
 let hungerCounter = document.querySelector('.hunger')
@@ -133,6 +34,129 @@ let boredomCounter = document.querySelector('.boredom')
 
 
 buttonBegin = document.querySelector('button');
+
+
+
+
+const wolfie = new Wolfie('.Wolfie')
+
+const game = {
+
+    name: wolfie.name,
+    hunger: wolfie.hunger,
+    sleepiness: wolfie.sleepiness,
+    boredom: wolfie.boredom,
+    age: wolfie.age,
+
+    setHunger(){
+        const intervalID = setInterval(() => {
+          const hungerID = document.querySelector('.hunger');
+          hungerID.innerText = ` ${wolfie.hunger}`;
+          // decrement hunger
+          wolfie.hunger--
+          if(wolfie.hunger <= 0){
+            alert('Your pet died from starvation')
+            document.querySelector('.pet').src = ('https://media0.giphy.com/media/3oEjHJYwFLAPyMx128/giphy.gif?cid=ecf05e47gbw11mmxpf2b6p4c7fovz8khp40mat33tb7xwh2h&rid=giphy.gif&ct=g')
+            clearInterval(intervalID);
+            document.querySelector('button').disabled = false;
+            return;
+          }
+        }, 1000)
+        hungerButton.addEventListener('click', () =>{
+            wolfie.feed();
+            console.log(wolfie.hunger);
+            document.body.style.backgroundImage = "url('https://64.media.tumblr.com/c4ef683055d8fc8e5517cdc221a7d57a/d3238a119a00c682-b9/s1280x1920/324855e88fa138547b29b3f87200161526dc2356.png')"
+            document.querySelector('.pet').src = ('https://i.imgur.com/RHnnBBA.png')
+          })
+      },
+      setSleepiness(){
+          const intervalID = setInterval(() => {
+            const sleepinessID = document.querySelector('.sleepiness');
+            sleepinessID.innerText = ` ${wolfie.sleepiness}`;
+            // decrement sleepiness
+            wolfie.sleepiness--
+            sleepinessButton.addEventListener('click', () =>{
+                wolfie.sleepiness++
+                console.log(wolfie.sleepiness);
+                document.body.style.backgroundImage = "url('https://cutewallpaper.org/21/pixel-stars-background/night-nighttime-stars-clouds-background-pixel-pixel-bac.png')";
+                document.querySelector('.pet').src = ('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTynq9Rj_tz0RJByVWNcKmhppzXA6e2uvs4Cg&usqp=CAU')
+                const pet = document.querySelector('.pet')
+            })
+            if(this.sleepiness <= 0){
+              alert('Your pet died from lack of rest')
+              //update image to dead wolf or game of thrones red wedding
+              document.querySelector('.pet').src = ('https://media0.giphy.com/media/3oEjHJYwFLAPyMx128/giphy.gif?cid=ecf05e47gbw11mmxpf2b6p4c7fovz8khp40mat33tb7xwh2h&rid=giphy.gif&ct=g')
+
+              clearInterval(intervalID);
+              document.querySelector('button').disabled = false;
+              return;
+            }
+          }, 5000)
+        },
+        setBoredom(){
+          const intervalID = setInterval(() => {
+            const boredomID = document.querySelector('.boredom');
+            boredomID.innerText = ` ${wolfie.boredom}`;
+            // decrement boredom
+            wolfie.boredom--
+            boredomButton.addEventListener('click', () =>{
+                wolfie.boredom++
+                document.body.style.backgroundImage = "url('https://64.media.tumblr.com/c4ef683055d8fc8e5517cdc221a7d57a/d3238a119a00c682-b9/s1280x1920/324855e88fa138547b29b3f87200161526dc2356.png')"
+                document.querySelector('.pet').src = ('https://i.imgur.com/RHnnBBA.png')
+              })
+            if(wolfie.boredom <= 0){
+              alert('Your pet died from an insane amount of boredom')
+              document.querySelector('.pet').src = ('https://media0.giphy.com/media/3oEjHJYwFLAPyMx128/giphy.gif?cid=ecf05e47gbw11mmxpf2b6p4c7fovz8khp40mat33tb7xwh2h&rid=giphy.gif&ct=g')
+
+              clearInterval(intervalID);
+              document.querySelector('button').disabled = false;
+              return;
+            }
+          }, 5000)
+        },
+
+          setAge(){
+            const intervalID = setInterval(() => {
+              const ageID = document.querySelector('.age');
+              ageID.innerText = ` ${wolfie.age}`;
+              // increment age
+              wolfie.age++
+          }, 7000)
+        },
+
+          setName(){
+          this.name = prompt('Name your pet')
+          const nameID = document.querySelector('.name')
+          nameID.innerText = `${this.name}`
+        },
+
+        secondForm(){
+          if(this.age === 2){
+            alert('Your pet has gained a new form!!!!')
+            document.querySelector('.pet').src = ('https://e7.pngegg.com/pngimages/159/420/png-clipart-blood-dog-red-fox-drawing-pack-pixel-art-wolf-horse-mammal.png')
+          }
+        },
+
+        thirdForm(){
+          if(this.age === 5){
+            alert('Your pet has gained another new form!!!!')
+            document.querySelector('.pet').src = ('https://w7.pngwing.com/pngs/595/697/png-transparent-black-wolf-illustration-wolfdog-panthera-deer-drawing-wolf-animals-dog-like-mammal-wolf.png')
+          }
+        },
+
+
+    startGame() {
+      this.setName()
+      this.setHunger()
+      this.setBoredom()
+      this.setSleepiness()
+      this.setAge()
+      this.secondForm()
+  }
+
+}
+
+
 
 
 buttonBegin.addEventListener("click", (event)=>{
