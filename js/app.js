7a7121507654ed39bd623fb23dcd3648b0f6094a
marcwolfe <marc.wolfe21@gmail.com>
2021-12-08 21:08:17 -0600

    Working on getting picture permissions to load in live website

--- js/app.js
+++ js/app.js
@@ -28,25 +28,8 @@ class Wolfie extends Pet {
 
   play(){
     this.boredom++
-    this.myMove()
   }
 
-//   myMove(){
-//     var id = null;
-//     var elem = document.querySelector(".pet");
-//     var pos = 0;
-//     clearInterval(id);
-//     id = setInterval(frame, 10);
-//     function frame() {
-//     if (pos == 350) {
-//       clearInterval(id);
-//     } else {
-//       pos++;
-//       elem.style.top = pos + 'px';
-//       elem.style.left = pos + 'px';
-//     }
-//   }
-// }
 }
 
 let hungerButton = document.querySelector('.feedButton')
